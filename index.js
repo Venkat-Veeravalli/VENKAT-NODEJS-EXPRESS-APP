48507cd23bf6377c3d0e9e3819c42737578ac553
Venkat-Veeravalli <venkatveeravalli91@gmail.com>
2023-09-28 16:17:47 +0530

    add express to the application

--- index.js
+++ index.js
@@ -1,106 +1,164 @@
-const request = require("request");
 const express = require('express');
-const path = require('path')
+const bodyParser = require('body-parser');
+const request = require('request');
+const passwordHash = require('password-hash');
+
 const app = express();
-app.use(express.static("public"));
+const port = 3000;
+
+
 const admin = require('firebase-admin');
-var serviceAccount = require("./key.json")
+const serviceAccount = require('./key.json');
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-app.set('view engine','ejs');
-
-app.use('/static',express.static(path.join(__dirname,'public')))
-app.use('/assets',express.static(path.join(__dirname,'public/assets')))
-
 const db = admin.firestore();
 
-app.get('/',function (req,res) {
-  res.render( __dirname + "/views/" + "dash" );
-});
-app.get('/dash',function (req,res) {
-  res.render( __dirname + "/views/" + "dash" );
-});
-app.get('/venkat',function (req,res) {
-  res.render( __dirname + "/views/" + "venkat" );
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
+
+app.use('/static', express.static(__dirname + '/public'));
+app.use('/assets', express.static(__dirname + '/public/assets'));
+
+app.get('/', (req, res) => {
+  res.render(__dirname + '/views/dash');
 });
 
-app.get('/services',function (req,res) {
-  res.render( __dirname + "/views/" + "services" );
+app.get('/dash', (req, res) => {
+  res.render(__dirname + '/views/dash');
 });
 
-app.get('/about',function (req,res) {
-  res.render( __dirname + "/views/" + "about" );
+app.get('/venkat', (req, res) => {
+  res.render(__dirname + '/views/venkat');
 });
 
-app.get('/signup',function (req,res) {
-  res.render( __dirname + "/views/" + "signup.ejs" );
+app.get('/services', (req, res) => {
+  res.render(__dirname + '/views/services');
 });
 
-app.get('/signupsubmit',function (req, res) {
-  db.collection("Users")
-    .add({
-      username: req.query.username,
-      email: req.query.email,
-      password: req.query.password,
-      CPassword: req.query.CPassword,
+app.get('/about', (req, res) => {
+  res.render(__dirname + '/views/about');
+});
+
+app.get('/signup', (req, res) => {
+  res.render(__dirname + '/views/signup.ejs');
+});
+
+app.post('/signupsubmit', (req, res) => {
+  const email = req.body.email;
+  const username = req.body.username;
+
+  
+  db.collection('UsersNew')
+    .where('email', '==', email)
+    .get()
+    .then((emailDocs) => {
+      db.collection('UsersNew')
+        .where('username', '==', username)
+        .get()
+        .then((usernameDocs) => {
+          if (emailDocs.size > 0) {
+            res.render('wrong');
+          } else if (usernameDocs.size > 0) {
+            res.render('wrong');
+          } else {
+          
+            db.collection('UsersNew')
+              .add({
+                username: username,
+                email: email,
+                password: passwordHash.generate(req.body.password),
+                CPassword: passwordHash.generate(req.body.CPassword),
+              })
+              .then(() => {
+                res.render('signup.ejs');
+              })
+              .catch(() => {
+                res.send('Something went wrong.');
+              });
+          }
+        })
+        .catch(() => {
+          res.send('Something went wrong.');
+        });
     })
-    .then(() => {
-      res.render("signup.ejs");
+    .catch(() => {
+      res.send('Something went wrong.');
     });
 });
 
-app.get('/home',function (req,res) {
-  res.render( __dirname + "/views/" + "home.ejs" );
+app.get('/home', (req, res) => {
+  res.render(__dirname + '/views/home.ejs');
 });
 
-app.get('/loginsubmit',function (req, res) {
-  db.collection("Users")
-  .where("email", "==" ,req.query.email)
-  .where("password", "==" ,req.query.password)
-  .get()
-  .then((docs) =>{
-    if(docs.size>0){
-      res.render("home.ejs");
-    }else{
-      res.render("fail.ejs");
-    }
-    console.log(docs.size)
-  });
-});
+app.post('/loginsubmit', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
 
-app.get('/weathersubmit',(req,res) =>{
-  const location = req.query.location;
-  request(
-    'https://api.weatherapi.com/v1/forecast.json?key=1d38e74f95ff438e84b64154220306&q='+location+'&days=7', function (error, response, body){
-      if("error" in JSON.parse(body))
-      {
-        if((JSON.parse(body).error.code.toString()).length > 0)
-        {
-          res.render("home.ejs");
+  db.collection('UsersNew')
+    .where('email', '==', email)
+    .get()
+    .then((docs) => {
+      if (docs.size === 0) {
+        res.render('fail.ejs'); 
+      } else {
+      
+        const userDoc = docs.docs[0];
+        const hashedPassword = userDoc.data().password;
+        if (passwordHash.verify(password, hashedPassword)) {
+          res.render('home.ejs'); 
+        } else {
+          res.render('fail.ejs');
         }
       }
-      else
-      {
-        
-        const country= JSON.parse(body).location.country;
+    })
+    .catch(() => {
+      res.send('Something went wrong.');
+    });
+});
+
+app.post('/weathersubmit', (req, res) => {
+  const location = req.body.location;
+  request(
+    'https://api.weatherapi.com/v1/forecast.json?key=1d38e74f95ff438e84b64154220306&q=' +
+      location +
+      '&days=7',
+    function (error, response, body) {
+      if ('error' in JSON.parse(body)) {
+        if (JSON.parse(body).error.code.toString().length > 0) {
+          res.render('home.ejs');
+        }
+      } else {
+        const country = JSON.parse(body).location.country;
         const loctime = JSON.parse(body).location.localtime;
         const temp_c = JSON.parse(body).current.temp_c;
         const temp_f = JSON.parse(body).current.temp_f;
         const wind_kph = JSON.parse(body).current.wind_kph;
         const humi = JSON.parse(body).current.humidity;
-       
-        res.render('location',{location:location,country:country,loctime:loctime,temp_c:temp_c,temp_f:temp_f,wind_kph:wind_kph,humi:humi});
-      } 
+
+        res.render('location', {
+          location: location,
+          country: country,
+          loctime: loctime,
+          temp_c: temp_c,
+          temp_f: temp_f,
+          wind_kph: wind_kph,
+          humi: humi,
+        });
+      }
     }
-    );
+  );
 });
 
-app.get('/locsubmit', (req, res) => {
-  res.render("weather");
+app.post('/locsubmit', (req, res) => {
+  res.render('weather');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
